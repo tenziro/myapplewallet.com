a5b6df3ad31aebc1d3348c11a9c8a897d6c497a9
tenziro <tenziro@naver.com>
2022-08-23 17:22:12 +0900

    스크팁트 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,5 @@
 
+// 전체 메뉴 컨트롤
 function menuControl() {
   const menu = document.querySelector('#menu');
   if (!menu.classList.contains('actived')) {
@@ -7,3 +8,28 @@ function menuControl() {
     menu.classList.remove('actived');
   }
 }
+
+// 헤더 컨트롤
+window.addEventListener("scroll", (event) => {
+  let header = document.querySelector('#header');
+  let scroll = this.scrollY;
+  if (scroll > 10 ) {
+    header.classList.add('blur');
+  } else {
+    header.classList.remove('blur');
+  }
+});
+
+// 슬라이더
+$(document).ready(function(){
+  $(".review-contents").owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    // autoplaySpeed: 10000,
+    autoplayHoverPause: false,
+    // mouseDrag: false,
+    smartSpeed: 0,
+  });
+});
